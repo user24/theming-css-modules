a0a9762f51b515036e9a798672f91a8e546a9f60
Howard Yeend <howard.yeend@gmail.com>
2023-07-17 12:02:25 +0100

    A barebones, unthemed Next app

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        I am not themed yet.
+      </div>
+    </main>
+  )
+}
