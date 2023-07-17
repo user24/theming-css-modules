359747177bc4b4b7748473838e43090bea36eaae
Howard Yeend <howard.yeend@gmail.com>
2023-07-17 12:29:49 +0100

    next-themes installed, doing nothing

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
-import styles from './page.module.css'
+'use client';
+
+import classes from './page.module.css';
+import { useTheme } from 'next-themes';
 
 export default function Home() {
+  const {theme, setTheme} = useTheme();
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main className={classes.main}>
+      <div className={classes.description}>
         I am not themed yet.
+        <button onClick={() => setTheme('dank')}>Dank Theme</button>
+        <button onClick={() => setTheme(' ')}>Default Theme</button>
       </div>
     </main>
   )
